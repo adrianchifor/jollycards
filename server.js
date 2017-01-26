dbdcc216c970d00b51e798c6cc56b6826f953064
Adrian Chifor <adrian.chifor@onfido.com>
2017-01-26 17:12:52 +0000

    Added redis. Updates for dokku

--- server.js
+++ server.js
@@ -1,45 +1,32 @@
 var fs = require('fs');
 var express = require('express');
-var level = require('level');
+var redisLib = require('redis');
 var ejs = require('ejs');
 var multer  = require('multer');
 var uuid = require('uuid/v4');
 
 var app = express();
 
-var staticFolder = __dirname + '/../static/';
-var dbFolder = __dirname + '/../db/';
-
-if (!fs.existsSync(staticFolder)) {
-    fs.mkdirSync(staticFolder);
-}
-
-if (!fs.existsSync(dbFolder)) {
-    fs.mkdirSync(dbFolder);
-}
-
-var db = level(dbFolder + 'jollycards');
+var redis = redisLib.createClient(process.env.REDIS_URL);
 
 var galleryPage = ejs.compile(fs.readFileSync('pages/gallery.html', 'utf-8'));
 var newPage = ejs.compile(fs.readFileSync('pages/new.html', 'utf-8'));
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, staticFolder);
+        cb(null, '/static');
     },
     filename: function(req, file, cb) {
         var imageName = uuid();
 
-        db.get(req.params.event, function(err, value) {
-            var images = [];
-
-            if (value) {
-                images = value;
+        redis.get(req.params.event, function(err, values) {
+            if (values) {
+                redis.rpush(req.params.event, imageName);
+            } else {
+                var images = [];
+                images.push(imageName);
+                redis.set(req.params.event, images);
             }
-
-            images.push(imageName);
-
-            db.put(req.params.event, images);
 
             cb(null, imageName);
         });
@@ -48,25 +35,24 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage }).single('image');
 
-var url = 'http://localhost:8080';
+var url = 'http://jollycards.dokku.adrianchifor.com';
 var uploadToken = 'G9qIw6rb0Zo5325r8irkCBV6Gku874';
 
 app.use('/', express.static(__dirname + '/pages'));
-app.use('/static', express.static(staticFolder));
+app.use('/static', express.static('/static'));
 
 app.get('/:event', function(req, res) {
-    var images = '';
-
-    db.get(req.params.event, function(err, value) {
-        if (err) return;
-
-        for (var i = 0; i < value.length; i++) {
-            images += "<img src=\"" + url + "/static/" + value[i] + "\" class=\"m-p-g__thumbs-img\" />\n";
+    redis.get(req.params.event, function(err, values) {
+        var images = '';
+        if (values) {
+            for (var i = 0; i < values.length; i++) {
+                images += "<img src=\"" + url + "/static/" + values[i] + "\" class=\"m-p-g__thumbs-img\" />\n";
+            }
         }
-    });
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(galleryPage({ event: req.params.event.replace(/\+/g, ' '), images: images }));
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(galleryPage({ event: req.params.event.replace(/\+/g, ' '), images: images }));
+    });
 });
 
 app.get('/:event/new', function(req, res) {
@@ -93,4 +79,4 @@ app.get('*', function(req, res) {
     res.status(404).send('404 Page Not Found');
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
